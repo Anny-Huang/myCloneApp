0ac85fe2d379fe19d81eb5d1c0908ebe617ef4e1
AN-NI HUANG <118084497+Anny-Huang@users.noreply.github.com>
2025-05-28 23:11:33 -0600

    intial setup and add a top bar for app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
 
-export default function App() {
+export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topBar}>
+        <Pressable onPress={() => alert('I am done with the lab')}>
+          <Text style={styles.icon}>←</Text>
+        </Pressable>
+        <Text style={styles.topBarTitle}>Posts</Text>
+        <Text style={styles.icon}>⋯</Text>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'white',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  topBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderBottomWidth: 1,
+    marginTop: 35,
+    borderColor: 'gray',
+  },
+  topBarTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  icon: {
+    fontSize: 20,
+    width: 30,
+    textAlign: 'center',
+  }
 });
